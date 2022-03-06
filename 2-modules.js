171345e0e1e66611d1f8063fee263741bbfabe10
Nelson Uprety <nelsonuprety3@gmail.com>
2022-03-06 21:22:33 +0000

    Modules export and import

--- 2-modules.js
+++ 2-modules.js
@@ -5,6 +5,10 @@ const names = require('./3-firstModule.js');
 const sayHi = require('./4-secondModule.js');
 const data = require('./6-alternativeExportTechnique.js');
 
+// When we are setting up everything if we have a function inside of the module that we invoke
+// , yes that code will run even though we didnt assign it to a variable
+require('./5-mind-grenade');
+
 console.log(names);
 sayHi('Nelson');
 sayHi(names.monkey);
