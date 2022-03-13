c10da9ee64136a3581faf9a78cc4681cc20295d2
Nelson Uprety <nelsonuprety3@gmail.com>
2022-03-13 19:10:26 +0000

    Async Await patterns

---
+++ 1-async-patterns/1-readFile.js
@@ -0,0 +1,39 @@
+// we can do this instead of util
+// const { readFile, writeFile } = require('fs').promises
+
+const { readFile, writeFile } = require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+  try {
+    const first = await readFilePromise('../content/first.txt', 'utf-8');
+    const second = await readFilePromise('../content/second.txt', 'utf-8');
+    await writeFilePromise(
+      '../content/result-from-asyncawait.txt',
+      `THIS IS AWESOME: ${first} ${second}`
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// const getText = path => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf-8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText('../content/first.txt')
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err));
